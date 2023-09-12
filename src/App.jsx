1f3bf3958a8ef3ff8c9693af78f5b30e742b07da
IsabelleCruz333 <isabellecruz333@gmail.com>
2023-09-12 10:15:54 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import "./App.scss";
+function App() {
+
+  return (
+     <>
+      <div className="container">
+        <section className="estrutura">
+          <article className="box">
+            <h1>Nossa estrutura</h1>
+            <img src="http://lorempixel.com.br/350/200/?1" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+          </article>
+          <article className="box">
+            <h1>Nossa estrtura</h1>
+            <img src="http://lorempixel.com.br/350/200/?2" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+          </article>
+        </section>
+      </div>
+
+      </>
+  )
+}
+
+export default App
